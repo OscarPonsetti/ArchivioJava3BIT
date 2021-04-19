e3fa2753a8573a989944656a094886a5209e4e2b
oscar <oscar@oscarprogram>
2021-04-19 13:08:34 +0200

    Classe Archivio 19/04/2021

--- ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
@@ -1,13 +1,32 @@
 package archiviojava3bit;
 
+import java.util.ArrayList;
+
 /**
  * Contiene gli oggetti del programma.
  * Implementata tramite un ArrayList.
  * Ci permette di inserire gli oggetti,
  * effettuare ricerche e
  * aggiungere/rimuovere/modificare un oggetto.
- * @author INSERISCI_NOME
+ * @author Oscar Ponsetti
  */
-public class Archivio {
+public class Archivio 
+{  
+    
+    //creo un arrayList che conterrà tutti i ricambi dell'archivio    
+    private ArrayList <Ricambio>  magazzino;
+
+    //Facciamo il costruttore
+
+    public Archivio()
+        {
+        //Ho creato l'array list che è vuoto
+        magazzino = new ArrayList(0);
+        }
+
+
+
+
+
     
 }
