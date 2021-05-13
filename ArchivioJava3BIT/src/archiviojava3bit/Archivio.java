979aceccf2995a7fec0e81728cc487df903f25ef
oscar <oscar@oscarprogram>
2021-05-13 16:24:13 +0200

    Classe Archivio apertura file, salvre nel file, chiusura 13/05/2021

--- ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
@@ -1,6 +1,13 @@
 package archiviojava3bit;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Contiene gli oggetti del programma.
@@ -22,6 +29,12 @@ public class Archivio
         {
         //Ho creato l'array list che è vuoto
         magazzino = new ArrayList(0);
+        
+        
+        //Lo riempiamo
+        magazzino = leggiDaFile();
+        
+        
         }
 
  //Mi permette di inserire un articolo nel magazzino (gli passiamo un oggetto di tipo ricambio)
@@ -66,8 +79,10 @@ public boolean elimina(String codice)
         {
         magazzino.remove(ricambio);
    
-        //Salvo tutto nel file ( da vedere)
-    
+        //Salvo tutto (l'ArrayList) nel file 
+        
+        SalvaSuFile();
+        
         return true;
         }
     
@@ -93,6 +108,7 @@ public boolean elimina(String codice)
             }
         }    
     return s;    
+    //Questo metodo converte l'articolo in stringa 
     }
 
 //Metodo che visualizza l'elenco degli articoli
@@ -184,9 +200,113 @@ public ArrayList<Ricambio> cercaRicambioNome(String nome)
     //Restituisco l'ArrayList  
     return articoloNome;
     }
+//Metodo per restituisre un array list di tipo ricambio dal file
 
 
 
+
+private ArrayList<Ricambio> leggiDaFile() 
+    {
+        //Eccezzioni try e catch
+        //Creaiamo un ArrayList vuoto
+            ArrayList<Ricambio> articoli;
+            articoli = new ArrayList();
+            
+        try {
+            
+            
+            //Usiamo l'oggetto di tipo file reader (gli passiamo il nome del file)
+            FileReader fileReader;
+            
+            //Cotruiamo il fileReader e gli passiamo
+            
+            fileReader = new FileReader ("archivio.csv");
+           
+            //Creiamo l'oggetto buffer reader
+            
+            BufferedReader input;
+            input = new BufferedReader(fileReader);
+            
+            //Leggiamo linae per linea
+            
+            String linea;
+            
+            //Legge la linea dal file e la assegna alla linea, se è uguale a null (niente) esce dal file (perché arrivato alla fine delle righe)
+            while( (linea = input.readLine()) != null)
+                {
+                String campi[];
+                
+                //Dividiamo i campi 
+                
+                campi = linea.split(",");
+                
+                //Es in campi[0] ci sarà il codice dell'articolo
+                //Eh via così per campi[ ......]
+                
+                //Converto le due stringhe in intero per la quantità, e in float per il prezzo
+                int quantita = Integer.parseInt(campi[1]);
+                float prezzo = Float.parseFloat(campi[6]);
+                float peso = Float.parseFloat(campi[3]);
+                
+                
+                //Ora creo l'articolo
+                
+                Ricambio ricambio = new Ricambio(campi[0], quantita, campi[2], peso, campi[4], campi[5], prezzo);
+                
+                magazzino.add(ricambio);
+                }   
+            
+            
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    //Diamo l'array in return
+        return magazzino;  
+    }
+
+    //Metodo che salva tutti i dati nel file archivio.csv
+    
+    private void SalvaSuFile()
+        {
+        try {
+            //Prende l'oggetto magazzino e lo salva sotto forma di stringa nel file (usiamo il metodo che abbiamo costruito toString)
+            
+            //Creiamo un oggetto per  scrivere sul file
+            FileWriter out;
+            
+            //Mettiamo tutto in un try-catch
+            
+            
+            out = new FileWriter("archivio.csv");
+            
+            //Stringa archivio
+            String sArchivio;
+            
+            //Richiamiamo il metodo toString
+            sArchivio = toString();
+            
+            
+            //Ora scriviamo nel file la stringa
+            
+            out.write(sArchivio);
+            
+            //Chiudo il file
+            
+            out.close();
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+            
+         
+        }
 
 
 }
